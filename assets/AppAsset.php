c80df6a8a7a40a4530b510384d5151fbaa240102
Henry Vega <hp.vega@hotmail.com>
2017-08-30 20:21:28 -0500

    Agenda

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,24 +20,11 @@ class AppAsset extends AssetBundle
     public $css = [
         'repository/bootstrap/dist/css/bootstrap.min.css',
         'repository/font-awesome/css/font-awesome.min.css',
-        'repository/animate.css/animate.min.css',
-        'repository/bootstrap-daterangepicker/daterangepicker.css',
         'css/custom.min.css',
-        'css/custom.css',
     ];
     public $js = [
         'js/agenda.js',
         'repository/bootstrap/dist/js/bootstrap.min.js',
-        'repository/Chart.js/dist/Chart.min.js',
-        'repository/gauge.js/dist/gauge.min.js',
-        'repository/bootstrap-progressbar/bootstrap-progressbar.min.js',
-        'repository/Flot/jquery.flot.js',
-        'repository/Flot/jquery.flot.time.js',
-        'repository/Flot/jquery.flot.resize.js',
-        'repository/flot-spline/js/jquery.flot.spline.min.js',
-        'repository/DateJS/build/date.js',
-        'repository/jqvmap/examples/js/moment.min.js',
-        'repository/bootstrap-daterangepicker/daterangepicker.js',
         'js/custom.min.js',
     ];
     public $depends = [
