ee1483374e7917969a61656838cf169637610d04
Henry Vega <hvega@mandu.com>
2017-09-04 16:39:39 -0500

    Agenda

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,42 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     uglify: {
+      options: {
+        compress: true
+      },
       dist: {
         src: 'web/js/agenda.js',
         dest: 'web/js/agenda.min.js'
       }
+    },
+    less: {
+      options: {
+        compress: true
+      },
+      dist: {
+        src: 'web/css/custom.css',
+        dest: 'web/css/custom.less'
+      }
+    },
+    watch: {
+      less: {
+        files: ['web/css/*.less'],
+        tasks: ['less'],
+        options: {
+          livereload: true
+        }
+      },
     }
+
   })
 
-  grunt.loadNpmTasks('grunt-typescript');
-  grunt.loadNpmTasks('grunt-concat-sourcemap');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-typescript')
+  grunt.loadNpmTasks('grunt-concat-sourcemap')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-less')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-copy')
 
-  grunt.registerTask('build', ['less', 'typescript', 'copy', 'concat_sourcemap', 'uglify']);
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['less', 'uglify'])
+  grunt.registerTask('default', ['watch'])
 }
