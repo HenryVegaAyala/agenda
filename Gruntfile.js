d4412713ed28fc934a4f53a0730f822cf0435270
Henry Vega <hp.vega@hotmail.com>
2017-09-13 00:29:33 -0500

    Agenda

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,35 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    // uglify: {
-    //   options: {
-    //     compress: true,
-    //     separator: ';'
-    //   },
-    //   dist: {
-    //     src: ['web/js/agenda.js', 'web/js/custom.js'],
-    //     dest: 'web/js/ticket.js'
-    //   }
-    // },
     uglify: {
       options: {
         compress: true,
-        separator: ';'
+        separator: ";"
       },
       dist: {
-        src: 'web/js/agenda.js',
-        dest: 'web/js/agenda.min.js'
+        src: "web/js/agenda.js",
+        dest: "web/js/agenda.min.js"
       }
     },
     less: {
       options: {
         compress: true,
-        style: 'expanded'
+        style: "expanded"
       },
       dist: {
-        src: 'web/css/custom.css',
-        dest: 'web/css/custom.scss'
+        src: "web/css/custom.css",
+        dest: "web/css/custom.scss"
       }
     },
   }),
 
-    grunt.loadNpmTasks('grunt-typescript'),
-    grunt.loadNpmTasks('grunt-concat-sourcemap'),
-    grunt.loadNpmTasks('grunt-contrib-watch'),
-    grunt.loadNpmTasks('grunt-contrib-less'),
-    grunt.loadNpmTasks('grunt-contrib-uglify'),
-    grunt.loadNpmTasks('grunt-contrib-copy'),
-    grunt.loadNpmTasks('grunt-contrib-sass'),
-    grunt.loadNpmTasks('grunt-contrib-concat'),
+    grunt.loadNpmTasks("grunt-typescript"),
+    grunt.loadNpmTasks("grunt-concat-sourcemap"),
+    grunt.loadNpmTasks("grunt-contrib-watch"),
+    grunt.loadNpmTasks("grunt-contrib-less"),
+    grunt.loadNpmTasks("grunt-contrib-uglify"),
+    grunt.loadNpmTasks("grunt-contrib-copy"),
+    grunt.loadNpmTasks("grunt-contrib-sass"),
+    grunt.loadNpmTasks("grunt-contrib-concat"),
 
-    grunt.registerTask('build', ['less', 'uglify'])
+    grunt.registerTask("build", ["less", "uglify"])
 }
