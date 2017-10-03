c65f79902af9bdb2098dda1f31e91179c4f80a87
Henry Vega <hp.vega@hotmail.com>
2017-10-03 00:03:22 -0500

    Agenda

--- views/user/_form.php
+++ views/user/_form.php
@@ -10,12 +10,7 @@ use yii\widgets\Pjax;
 /* @var $form yii\widgets\ActiveForm */
 
 $descripcion = "Registrar Usuario";
-?>
-<div class="clearfix"></div>
-<div class="row">
-    <div class="col-md-12 col-sm-12 col-xs-12">
-        <div class="x_panel">
-            <div class="x_content"> <?php Pjax::begin(); ?> <?php $form = ActiveForm::begin(
+?> <div class="clearfix"></div><div class="row"><div class="col-md-12 col-sm-12 col-xs-12"><div class="x_panel"><div class="x_content"> <?php Pjax::begin(); ?> <?php $form = ActiveForm::begin(
                     [
                         'enableAjaxValidation' => false,
                         'enableClientValidation' => true,
@@ -26,49 +21,21 @@ $descripcion = "Registrar Usuario";
                             'data-pjax' => true,
                         ],
                     ]
-                ); ?> <span class="section"><?php echo Html::encode($descripcion) ?></span>
-                <div class="row">
-                    <div class="item form-group">
-                        <div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model, 'nombres')->textInput(
+                ); ?> <span class="section"><?php echo Html::encode($descripcion) ?></span><div class="row"><div class="item form-group"><div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model, 'nombres')->textInput(
                                 ['maxlength' => true],
                                 ['class' => 'form-control col-md-7 col-xs-12']
-                            ) ?> </div>
-                        <div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model, 'correo')->textInput(
+                            ) ?> </div><div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model, 'correo')->textInput(
                                 ['maxlength' => true],
-                                ['class' => 'form-control col-md-7 col-xs-12']) ?> </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="item form-group">
-                        <div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model, 'contrasena')->passwordInput(
+                                ['class' => 'form-control col-md-7 col-xs-12']) ?> </div></div></div><div class="row"><div class="item form-group"><div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model, 'contrasena')->passwordInput(
                                 ['maxlength' => true],
                                 ['class' => 'form-control col-md-7 col-xs-12']
-                            ) ?> </div>
-                        <div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model,
+                            ) ?> </div><div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model,
                                 'contrasena_desc')->passwordInput(
                                 ['maxlength' => true],
-                                ['class' => 'form-control col-md-7 col-xs-12']) ?> </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="item form-group">
-                        <div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model,
+                                ['class' => 'form-control col-md-7 col-xs-12']) ?> </div></div></div><div class="row"><div class="item form-group"><div class="col-md-6 col-sm-6 col-xs-12"> <?= $form->field($model,
                                 'estado')->dropDownList(Utils::status(), [
                                 'prompt' => 'Seleccionar Estado',
                                 'class' => 'form-control col-md-7 col-xs-12',
-                            ]) ?> </div>
-                    </div>
-                </div>
-            </div>
-            <div class="ln_solid"></div>
-            <div class="container-fluid">
-                <div class="form-group">
-                    <center>
-                        <div class="col-md-6 col-md-offset-3"> <?= Html::submitButton('<i class="fa fa-floppy-o fa-lg"></i> ' . ' Guardar',
+                            ]) ?> </div></div></div></div><div class="ln_solid"></div><div class="container-fluid"><div class="form-group"><center><div class="col-md-6 col-md-offset-3"> <?= Html::submitButton('<i class="fa fa-floppy-o fa-lg"></i> ' . ' Guardar',
                                 ['class' => 'btn btn-success']) ?> <?= Html::resetButton('<i class="fa fa-times fa-lg"></i> ' . ' Cancelar',
-                                ['class' => 'btn btn-primary']) ?> </div>
-                    </center>
-                </div>
-            </div> <?php ActiveForm::end(); ?> <?php Pjax::end(); ?> </div>
-    </div>
-</div>
+                                ['class' => 'btn btn-primary']) ?> </div></center></div></div> <?php ActiveForm::end(); ?> <?php Pjax::end(); ?> </div></div></div>
