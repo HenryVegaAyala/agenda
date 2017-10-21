5a02e658590201c6109eca50d2c4f7249a4890a9
Henry Vega <hp.vega@hotmail.com>
2017-10-21 13:02:49 -0500

    Agenda

--- views/user/_export.php
+++ views/user/_export.php
@@ -6,7 +6,7 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\Cliente */
 /* @var $form yii\widgets\ActiveForm */
-$descripcion = "Importar/Exportar Clientes";
+$descripcion = "Exportar Analistas  ";
 ?>
 <div class="clearfix"></div>
 <div class="row">
