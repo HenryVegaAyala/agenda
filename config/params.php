6d29ed3398c4ae1f27381035f95f3ce8e400b639
Henry Vega <hp.vega@hotmail.com>
2017-12-10 16:32:46 -0500

    ticket

--- config/params.php
+++ config/params.php
@@ -1,13 +1,13 @@
 <?php
 
-Yii::setAlias('@LogoHombreDefault', 'agenda/images/usuario_hombre.png');
-Yii::setAlias('@LogoMujerDefault', 'agenda/images/usuario_mujer.png');
-Yii::setAlias('@LogoDefault', 'agenda/images/usuario_default.png');
-Yii::setAlias('@ExcelImport', 'agenda/images/excel_import.png');
-Yii::setAlias('@ExcelDownload', 'agenda/images/excel_download.png');
+Yii::setAlias('@LogoHombreDefault', 'images/usuario_hombre.png');
+Yii::setAlias('@LogoMujerDefault', 'images/usuario_mujer.png');
+Yii::setAlias('@LogoDefault', 'images/usuario_default.png');
+Yii::setAlias('@ExcelImport', 'images/excel_import.png');
+Yii::setAlias('@ExcelDownload', 'images/excel_download.png');
 Yii::setAlias('@PathReporteDownload',  'reporte');
 Yii::setAlias('@PathExcelImport', 'temp');
-Yii::setAlias('@PathImagenColaboradores', 'agenda/images/collaboradores');
+Yii::setAlias('@PathImagenColaboradores', 'images/collaboradores');
 
 return [
     'adminEmail' => 'admin@example.com',
